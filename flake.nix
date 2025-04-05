231fdfbce1d54a2ed35553e98c8ba60977fa64eb
Stefan <git@stefn.co.uk>
2025-04-05 17:57:46 +0000

    .

--- flake.nix
+++ flake.nix
@@ -1,108 +1,57 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    chobble-template = {
+      url = "git+https://git.chobble.com/chobble/chobble-template.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
+      chobble-template,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        nodeDeps = import ./node-deps.nix { inherit pkgs; };
-        inherit (nodeDeps) packageJSON nodeModules;
-
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        commonBuildInputs = with pkgs; [
-          html-tidy
-          sass
-          yarn
-        ];
-
-        site = pkgs.stdenv.mkDerivation {
-          name = "eleventy-site";
-          src = ./.;
-          buildInputs = commonBuildInputs ++ [ nodeModules ];
-
-          configurePhase = ''
-            ln -sf ${packageJSON} package.json
-            ln -sf ${nodeModules}/node_modules .
-          '';
-
-          buildPhase = ''
-            sass --update src/_scss:_site/css --style compressed
-            yarn --offline eleventy
-            find _site -name "*.html" -exec tidy --wrap 80 --indent auto --indent-spaces 2  --wrap 80 --quiet yes --tidy-mark no --drop-empty-elements no -modify {} \;
-          '';
-
-          installPhase = ''cp -r _site $out'';
-
-          # Fix potential permissions issues
-          dontFixup = true;
-        };
-
-        mkScript =
-          name:
-          (pkgs.writeScriptBin name (builtins.readFile ./bin/${name})).overrideAttrs (old: {
-            buildCommand = "${old.buildCommand}\n patchShebangs $out";
-          });
-
-        mkPackage =
-          name:
-          pkgs.symlinkJoin {
-            inherit name;
-            paths = [ (mkScript name) ] ++ commonBuildInputs;
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-          };
-
-        scripts = [
-          "build"
-          "serve"
-          "dryrun"
-          "tidy_html"
-        ];
-
-        scriptPackages = builtins.listToAttrs (
-          map (name: {
-            inherit name;
-            value = mkPackage name;
-          }) scripts
-        );
-      in
-      rec {
-        defaultPackage = packages.site;
-        packages = scriptPackages // {
-          inherit site;
-        };
-
-        devShells = rec {
-          default = dev;
-          dev = pkgs.mkShell {
-            buildInputs = commonBuildInputs ++ (map (name: mkPackage name) scripts);
-            shellHook = ''
-              rm -rf node_modules
-              rm -rf package.json
-              ln -sf ${packageJSON} package.json
-              ln -sf ${nodeModules}/node_modules .
-              echo "Development environment ready!"
-              echo ""
-              echo "Run:"
-              echo " - 'serve' to start development server"
-              echo " - 'build' to build the site in the _site directory"
-              echo " - 'dryrun' to do a dry run"
-              echo " - 'tidy_html' to tidy the html in _site"
-              echo ""
-              git pull
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          templatePackages = chobble-template.packages.${system};
+          pkgs = nixpkgs.legacyPackages.${system};
+          newSite = pkgs.stdenv.mkDerivation {
+            name = "chobble-template-site";
+            src = ./.;
+            buildInputs = templatePackages.site.buildInputs;
+            dontPatchShebangs = true;
+            phases = [
+              "unpackPhase"
+              "setupTemplatePhase"
+              "configurePhase"
+              "buildPhase"
+              "installPhase"
+            ];
+            setupTemplatePhase = ''
+              cp -r ${chobble-template}/* ./
+              cp -r ${chobble-template}/.[!.]* ./ 2>/dev/null || true
+              find src -name "*.md" -type f -delete
+              rm -rf src/images/*
             '';
+            configurePhase = templatePackages.site.configurePhase;
+            buildPhase = templatePackages.site.buildPhase;
+            installPhase = templatePackages.site.installPhase;
+            dontFixup = true;
           };
-        };
-      }
-    );
+        in
+        {
+          site = newSite;
+          default = newSite;
+        }
+      );
+      devShells = chobble-template.devShells;
+    };
 }
